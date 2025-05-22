3557beeb564e1d6cd8402f553e668f6621c41d95
JafetICM <jafet145a@gmail.com>
2025-05-22 14:34:49 -0600

    noveno

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(cors({
-  origin: ['https://tu-api.onrender.com']
-}));
+
 
 
 const visitantesRoutes = require('./routes/visitantes.routes');
