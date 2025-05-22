e493c5dba91be841b3e5cf991e26c99bf07ab29b
JafetICM <jafet145a@gmail.com>
2025-05-22 13:17:31 -0600

    cuarto

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.use('/api/usuarios', usuariosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}/api-docs`);
 });
